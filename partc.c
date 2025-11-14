046e1967b6dce2b8053aef0b8e5b4ccb8c59208a
Ryan Jaafari <jaafarir@wit.edu>
2025-11-14 14:45:32 -0500

    Completed Part C

--- partc.c
+++ partc.c
@@ -8,5 +8,39 @@
 int main(int argc, char* argv[]) {
     // TODO: Complete and document
 
-    return 0;
+    if (argc != 2) {
+        printf("USAGE: partc FILEIN\n");
+        return 1;
+    }
+
+    char *filename = argv[1];
+    int pipefd[2];
+
+    if (pipe(pipefd) < 0)
+        return 2;
+
+    pid_t pid = fork();
+    if (pid < 0)
+        return 3;
+
+    if (pid == 0) {
+        close(pipefd[0]);
+        if (dup2(pipefd[1], STDOUT_FILENO) < 0)
+            return 4;
+        close(pipefd[1]);
+        char *args[] = {"parta", filename, NULL};
+        execv("./parta", args);
+        return 5;
+    }
+
+    close(pipefd[1]);
+
+    if (dup2(pipefd[0], STDIN_FILENO) < 0)
+        return 6;
+    close(pipefd[0]);
+
+    char *sort_args[] = {"sort", "-t,", "-k2", "-n", NULL};
+    execvp("sort", sort_args);
+
+    return 7;
 }

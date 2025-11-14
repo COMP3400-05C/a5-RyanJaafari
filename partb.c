675fde7e5d41bdc98a22232eac106b1bae536a1f
Ryan Jaafari <jaafarir@wit.edu>
2025-11-14 14:42:48 -0500

    Completed Part B

--- partb.c
+++ partb.c
@@ -7,5 +7,37 @@
 
 int main(int argc, char* argv[]) {
     // TODO: Complete and document
+    if (argc != 3) {
+        printf("USAGE: partb FILEIN FILEOUT\n");
+        return 1;
+    }
+
+    char *input_filename = argv[1];
+    char *output_filename = argv[2];
+
+    int fd = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, 0770);
+    if (fd < 0) {
+        perror("open");
+        return 2;
+    }
+
+    if (dup2(fd, STDOUT_FILENO) < 0) {
+        perror("dup2");
+        close(fd);
+        return 3;
+    }
+    close(fd);
+
+    printf("Category,Count\n");
+    fflush(stdout);
+
+    char *parta_args[] = { "parta", input_filename, NULL };
+    int eret = execv("./parta", parta_args);
+
+    if (eret == -1) {
+        perror("execv");
+        return 4;
+    }
+
     return 0;
 }
